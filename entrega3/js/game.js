0ab163d8b57820f9b823ce8375eb21fd30ce63d3
unknown <Ricardo Gentil>
2022-10-23 13:31:07 -0300

    Se toman los valores para el inicio del juego desde las opcionesdel mismo. Se mejora visibilidad de las fichas

--- entrega3/js/game.js
+++ entrega3/js/game.js
@@ -27,4 +27,50 @@ function iniciarPagina() {
         heroTitleJugarOpciones.style.display = 'none';
     })
 
+
+    let btnPlayStart = document.querySelector("#btn-play-start");
+    btnPlayStart.addEventListener("click", function (evetn) {
+        //Display solo canvas
+        heroTitleJugar.style.display = 'none';
+        heroTitleJugarOpciones.style.display = 'none';
+
+        //Opciones seleccionadas
+        //Dificultad
+        let dificultad;
+        let dificultades = document.getElementsByName('dificultad');
+        for (let d of dificultades) {
+            if (d.checked) {
+                dificultad = d.value;
+            }
+        }
+        console.log(dificultad);
+
+        //Jugador 1
+        //Nombre
+        let nombre1 = document.getElementById('text-jugador1').value;
+        console.log(nombre1);
+        //Ficha
+        let ficha1;
+        let fichas1 = document.getElementsByName('targetgroup1');
+        for (let ficha of fichas1) {
+            if (ficha.checked) {
+                ficha1 = ficha.value;
+            }
+        }
+        console.log(ficha1);
+
+        //Jugador 2
+        //Nombre
+        let nombre2 = document.getElementById('text-jugador2').value;
+        console.log(nombre2);
+        //Ficha
+        let ficha2;
+        let fichas2 = document.getElementsByName('targetgroup2');
+        for (let ficha of fichas2) {
+            if (ficha.checked) {
+                ficha2 = ficha.value;
+            }
+        }
+        console.log(ficha2);
+    })
 }
