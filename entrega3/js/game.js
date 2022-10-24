850951e9ea1b014ffa892628af2f6153953cc654
unknown <Ricardo Gentil>
2022-10-24 09:45:14 -0300

    Se agrega timer de juego. Funciones para reiniciar y salir

--- entrega3/js/game.js
+++ entrega3/js/game.js
@@ -12,6 +12,9 @@ function iniciarPagina() {
     let nombre2;
     let ficha2;
 
+    let timer = 5;
+    let timerId;
+
     let divHeroGame = document.querySelector("#div-hero-game");
     divHeroGame.style.display = 'block';
 
@@ -35,33 +38,72 @@ function iniciarPagina() {
     //hasta aca
 
     let btnPlay = document.querySelector("#btn-play-cel");
-    btnPlay.addEventListener("click", function (evetn) {
+    btnPlay.addEventListener("click", function (event) {
         heroTitleJugar.style.display = 'none';
         heroTitleJugarOpciones.style.display = 'block';
-    })
+    });
 
     let btnCancel = document.querySelector("#btn-play-cancel");
-    btnCancel.addEventListener("click", function (evetn) {
+    btnCancel.addEventListener("click", function (event) {
         heroTitleJugar.style.display = 'block';
         heroTitleJugarOpciones.style.display = 'none';
-    })
+    });
 
     let btnGameOut = document.querySelector("#btn-game-out");
-    btnGameOut.addEventListener("click", function (evetn) {
+    btnGameOut.addEventListener("click", function (event) {
         divHeroGame.style.display = 'block';
         contentCanvas.style.display = 'none';
         canvas.style.display = 'none';
         heroTitleJugar.style.display = 'block';
         heroTitleJugarOpciones.style.display = 'none';
-    })
+    });
+
+    let resultadoCanvas = document.querySelector(".resultado-canvas");
 
     let btnReiniciar = document.querySelector("#btn-reiniciar");
-    btnReiniciar.addEventListener("click", function (evetn) {
+    btnReiniciar.addEventListener("click", function () {
+        
+        resultadoCanvas.style.display = 'none';
+        clearTimeout(timerId);
+
+        //Opciones seleccionadas
+        //Dificultad
+        let dificultades = document.getElementsByName('dificultad');
+        for (let d of dificultades) {
+            if (d.checked) {
+                dificultad = d.value;
+            }
+        }
+
+        //Jugador 1
+        //Nombre
+        nombre1 = document.getElementById('text-jugador1').value;
+        //Ficha
+        let fichas1 = document.getElementsByName('targetgroup1');
+        for (let ficha of fichas1) {
+            if (ficha.checked) {
+                ficha1 = ficha.value;
+            }
+        }
+        //Jugador 2
+        //Nombre
+        nombre2 = document.getElementById('text-jugador2').value;
+        //Ficha
+        ficha2;
+        let fichas2 = document.getElementsByName('targetgroup2');
+        for (let ficha of fichas2) {
+            if (ficha.checked) {
+                ficha2 = ficha.value;
+            }
+        }
+
         initDraw();
-    })
+        timer = 5;
+        decreaseTimer();
+    });
 
     let btnPlayStart = document.querySelector("#btn-play-start");
-    btnPlayStart.addEventListener("click", function (evetn) {
+    btnPlayStart.addEventListener("click", function () {
         //Display solo canvas
         heroTitleJugar.style.display = 'none';
         heroTitleJugarOpciones.style.display = 'none';
@@ -85,7 +127,6 @@ function iniciarPagina() {
         //Nombre
         nombre1 = document.getElementById('text-jugador1').value;
         //Ficha
-        let ficha1;
         let fichas1 = document.getElementsByName('targetgroup1');
         for (let ficha of fichas1) {
             if (ficha.checked) {
@@ -105,9 +146,9 @@ function iniciarPagina() {
         }
 
         initDraw();
-
+        timer = 5;
         decreaseTimer();
-    })
+    });
 
     function initDraw() {
         //Para dibujar el fondo
@@ -115,12 +156,17 @@ function iniciarPagina() {
         ctx.fillRect(0, 0, canvas.width, canvas.height);
     }
 
-    let timer = 60
-    function decreaseTimer(){
-        setTimeout(decreaseTimer,1000);
-        if (timer > 0){
-            timer --;
+    function decreaseTimer() {
+        if (timer > 0) {
+            timerId = setTimeout(decreaseTimer, 1000);
+            timer--;
             document.querySelector(".timer").innerHTML = timer;
+        }
+
+        if (timer === 0) {
+            clearTimeout(timerId);
+            resultadoCanvas.style.display = 'flex';
+            resultadoCanvas.innerHTML = 'Empate';
         }
     }
 }
