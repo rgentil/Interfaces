674aa872f8b5a57412513ec3606acdea0229b23d
Ricardo Gentil <rgentil2112@gmail.com>
2022-11-14 09:43:11 -0300

    Se corrige imagen del logo al scrollear

--- entrega4/js/menu.js
+++ entrega4/js/menu.js
@@ -1,6 +1,7 @@
 const burgerDiv = document.querySelector('#burgerDiv');
 const menuBurger = document.querySelector('.headerRigth .menu_opciones');
 const social = document.querySelector('.social');
+const logo = document.querySelector('.logo');
 
 burgerDiv.addEventListener('click', e => {
   burgerDiv.classList.toggle('active');
@@ -14,13 +15,11 @@ window.onscroll = function () {
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.querySelector(".nav-bar").style.height = "100px";
-    document.querySelector(".logo").style.display = 'none';
-    document.querySelector(".logoMin").style.display = 'block'
+    logo.classList.add('min');
     menuBurger.style.top = '100px';
   } else {
     document.querySelector(".nav-bar").style.height = '230px';
-    document.querySelector(".logo").style.display = 'block';
-    document.querySelector(".logoMin").style.display = 'none';
+    logo.classList.remove('min');
     menuBurger.style.top = '150px';
   }
 }
